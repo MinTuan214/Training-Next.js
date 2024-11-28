4557c258e709c93f810253c9b306859bcc309b23
Tuan <Tuan@gmail.com>
2024-11-28 17:24:33 +0700

    List user and detail

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,75 @@
-import Image from "next/image";
+"use client";
+
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [users, setUsers] = useState<User[]>([]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  useEffect(() => {
+    const fetchData = async () => {
+      const getUsers = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const data: User[] = await getUsers.json();
+      setUsers(data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="font-semibold text-lg mb-4 uppercase text-center">
+        User List
+      </h1>
+      <table className="table-auto border-collapse border border-gray-300 w-full text-sm text-left">
+        <thead>
+          <tr className="bg-gray-200">
+            <th className="border border-gray-300 px-4 py-2">Name</th>
+            <th className="border border-gray-300 px-4 py-2">Username</th>
+            <th className="border border-gray-300 px-4 py-2">Email</th>
+            <th className="border border-gray-300 px-4 py-2 text-center">
+              Actions
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.length === 0 ? (
+            <tr>
+              <td className="border border-gray-300 px-4 py-2 text-center" colSpan={4}>Loading...</td>
+            </tr>
+          ) : (
+            users.map((user, index) => (
+              <tr key={index}>
+                <td className="border border-gray-300 px-4 py-2">
+                  {user.name}
+                </td>
+                <td className="border border-gray-300 px-4 py-2">
+                  {user.username}
+                </td>
+                <td className="border border-gray-300 px-4 py-2">
+                  {user.email}
+                </td>
+                <td className="border border-gray-300 px-4 py-2 text-center">
+                  <Link
+                    href={`/users/${user.id}`}
+                    className="bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-600"
+                  >
+                    Detail
+                  </Link>
+                </td>
+              </tr>
+            ))
+          )}
+        </tbody>
+      </table>
     </div>
   );
 }
---
+++ src/app/users/[detail]/page.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  website: string;
+  address: string;
+};
+
+export default function UserDetail({params}: {params: Promise<{ detail: string }>}) {
+  const [id, setId] = useState<number>();
+  const [user, setUser] = useState<User>();
+
+  useEffect(() => {
+    const getId = async () => {
+      const getParams = await params; 
+      setId(Number(getParams.detail));
+    };
+    getId();
+  }, [params]);
+
+  useEffect(() => {
+    if (id) {
+      const fetchUser = async () => {
+        const getUser = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+        const data: User = await getUser.json();
+        setUser(data);
+      };
+      fetchUser();
+    }
+  }, [id]); 
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="font-semibold text-2xl mb-6 text-center">User Detail </h1>
+
+      <div className="bg-white p-6 rounded-lg shadow-lg">
+        {user ? (
+          <>
+            <div className="mb-4">
+              <h2 className="text-xl font-semibold text-gray-800">Id:</h2>
+              <p className="text-gray-600">{user.id}</p>
+            </div>
+            <div className="mb-4">
+              <h2 className="text-xl font-semibold text-gray-800">Name:</h2>
+              <p className="text-gray-600">{user.name}</p>
+            </div>
+
+            <div className="mb-4">
+              <h2 className="text-xl font-semibold text-gray-800">Username:</h2>
+              <p className="text-gray-600">{user.username}</p>
+            </div>
+
+            <div className="mb-4">
+              <h2 className="text-xl font-semibold text-gray-800">Email:</h2>
+              <p className="text-gray-600">{user.email}</p>
+            </div>
+
+            <div className="mb-4">
+              <h2 className="text-xl font-semibold text-gray-800">Phone:</h2>
+              <p className="text-gray-600">{user.phone}</p>
+            </div>
+
+            <div className="mb-4">
+              <h2 className="text-xl font-semibold text-gray-800">Website:</h2>
+              <p className="text-gray-600">{user.website}</p>
+            </div>
+          </>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    </div>
+  );
+}
